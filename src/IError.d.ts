a0125d9dbd901a20f68d7883095805d7e1fab189
Michael Leontieff <Michael.LEONTIEFF-SMITH@3ds.com>
2020-07-21 14:08:12 +1000

    doc changes

--- src/IError.d.ts
+++ src/IError.d.ts
@@ -1,20 +1,39 @@
+/**
+ * Error Payload
+ * 
+ * HTTP 400 = Validation Error, body will contain detail
+ * HTTP 500 = Execute Error
+ */
 export interface IError {
-    // HTTP error code
-    status: number;
-    detail: {
-        // The Id of the filter that threw the error
-        id: number;
-        // The component of the filter where the error originates
-        failure: Failure;
-        // the error message
-        message: string;
-    };
+    /**
+     * The error message
+     * 
+     * - If the error occurred at the filter level, a simple error fit for UI output
+     * will be returned.
+     * 
+     * - If a more general ParseException occurred, that trace will be returned. Unlike filter-level
+     * errors, these errors are raised with respect to a string GDSQL expression, not a query structure which
+     * is less malleable to a query builder UI.
+     */
+    message: string;
+
+    /**
+     * The component of the expression where the error originated
+     * 
+     * - If a filter error, the part of the filter that failed parsing.
+     * 
+     * - If a more general error, then "GENERAL"
+     */
+    type?: ErrorTypes;
+
+    // If the error was at the filter level (that could be represented in a UI)
+    // then the Id of the filter that threw the error will be contained here
+    filterId?: number;
+
 }
 
-
-type Failure = 
+type ErrorTypes = 
     "SUBJECT" |
     "PREDICATE" |
     "VALUE" |
-    // if the error is not particular to a filter, rather the composition of the expression (invalid operator, un-matched parenthesis etc.)
-    "GENERAL";
+    "GENERAL"
